0e157ab32c8906ca2203ab6cbdbcd49102500c57
Marcus Stollsteimer <sto.mar@web.de>
2014-04-25 13:31:39 +0200

    Refactor create_html.rb

--- orgeln-herrenberg/src/create_html.rb
+++ orgeln-herrenberg/src/create_html.rb
@@ -6,12 +6,26 @@ require "erb"
 require_relative "orgel"
 
 OUTPUT_DIR = "db"
-ORTE_TEMPLATE = "orte.html.erb"
-PAGE_TEMPLATE = "layout.html.erb"
-ERBAUER_TEMPLATE = "erbauer.html.erb"
-ORGELN_TEMPLATE = "dispositionen.html.erb"
+PAGE_TEMPLATE = ERB.new(File.read("layout.html.erb"), 0, '%<>-')
+orte_template = ERB.new(File.read("orte.html.erb"), 0, '%<>-')
+orte_outfile  = "orte.html"
+erbauer_template = ERB.new(File.read("erbauer.html.erb"), 0, '%<>-')
+erbauer_outfile  = "erbauer.html"
+orgeln_template = ERB.new(File.read("dispositionen.html.erb"), 0, '%<>-')
+orgeln_outfile  = "orgeldb.html"
 
 orgel_sammlung = OrgelSammlung.yaml_load("orgeldb.yaml")
+
+#####
+
+def render_and_save(template, outer_binding, title:, outfile:)
+  page_content = template.result(outer_binding)
+
+  page_title = title    # used in PAGE_TEMPLATE
+  page = PAGE_TEMPLATE.result(binding).gsub(/ +$/, "").gsub(/^ +/, "")
+
+  File.open(OUTPUT_DIR + "/" + outfile, "w") {|f| f.puts page }
+end
 
 #####
 
@@ -24,51 +38,34 @@ Dir.mkdir(OUTPUT_DIR)
 
 #####
 
-page_template = ERB.new(File.read(PAGE_TEMPLATE), 0, '%<>-')
-
 # Orte
 
+title = "Orgeln in Herrenberg / Ortschaften"
 orgeln = orgel_sammlung.sort_by_location
-page_title = "Orgeln in Herrenberg / Ortschaften"
-page_content = ERB.new(File.read(ORTE_TEMPLATE), 0, '%<>-').result(binding)
-page = page_template.result(binding)
-page.gsub!(/ +$/, "")
-page.gsub!(/^ +/, "")
-File.open(OUTPUT_DIR + "/" + ORTE_TEMPLATE.gsub(/\.erb/, ""), "w") {|f| f.puts page }
+
+render_and_save(orte_template, binding, title: title, outfile: orte_outfile)
 
 # Erbauer
 
+title = "Orgeln in Herrenberg / Orgelbauer"
 orgeln = orgel_sammlung.sort_by_builder
-page_title = "Orgeln in Herrenberg / Orgelbauer"
-page_content = ERB.new(File.read(ERBAUER_TEMPLATE), 0, '%<>-').result(binding)
-page = page_template.result(binding)
-page.gsub!(/ +$/, "")
-page.gsub!(/^ +/, "")
-File.open(OUTPUT_DIR + "/" + ERBAUER_TEMPLATE.gsub(/\.erb/, ""), "w") {|f| f.puts page }
+
+render_and_save(erbauer_template, binding, title: title, outfile: erbauer_outfile)
 
 # Dispositionen
 
+title = "Orgeln in Herrenberg"
 orgeln = orgel_sammlung.sort_by_location
-page_title = "Orgeln in Herrenberg"
-
-orgel_template = ERB.new(File.read(ORGELN_TEMPLATE), 0, '%<>-')
 
 ## alle
 
-page_content = orgel_template.result(binding)
-page = page_template.result(binding)
-page.gsub!(/ +$/, "")
-page.gsub!(/^ +/, "")
-File.open(OUTPUT_DIR + "/orgeldb.html", "w") {|f| f.puts page }
+render_and_save(orgeln_template, binding, title: title, outfile: orgeln_outfile)
 
 ## einzeln
 
 orgel_sammlung.each do |orgel|
-  orgeln = OrgelSammlung.new([orgel])
+  orgeln  = OrgelSammlung.new([orgel])
+  outfile = "#{orgel.id}.html"
 
-  page_content = orgel_template.result(binding)
-  page = page_template.result(binding)
-  page.gsub!(/ +$/, "")
-  page.gsub!(/^ +/, "")
-  File.open(OUTPUT_DIR + "/#{orgel.id}.html", "w") {|f| f.puts page }
+  render_and_save(orgeln_template, binding, title: title, outfile: outfile)
 end
